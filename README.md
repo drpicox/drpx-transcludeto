7b05e4b3d9d3620ec90e91b1ea3bac4d957327d3
David Rodenas <david.rodenas@gmail.com>
2015-03-08 21:51:41 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+drpx-transcludeTo
+=================
+
+An Angular directives/preLinkFunction generator to create multiple transcludes
+paired with transcludeTo/transcludeId (like Marionette region or ui-router
+siblings views).
+
+It pairs transcludes `target-to` elements into `target-id` elements inside templates, even when `ng-if`, `ng-repeat` and others are involved.
+
+
+Example/How to use
+------------------
+
+Imagine that you want to write something like this:
+
+```html
+<my-custom-list>
+    <h1 target-to="header">People</h1>
+    <div target-to="item">
+        {{$parent.item}}
+    </div>
+    <div target-to="footer">
+        <button ng-click="add()">Add</button>
+    </div>
+</my-custom-list>
+```
+
+With transcludeTo you should write `myCustomList` directive as follows:
+
+```javascript
+    app.directive('myCustomList', ['transcludeToPreLink', function(transcludeToPreLink) {
+        return {
+            restrict: 'E',
+            templateUrl: 'myCustomList.html',
+            transclude: true,                      // needs transclusion activate
+            scope: {},                             // needs own scope
+            link: {pre: transcludeToPreLink(), post: link},
+        };
+
+        function link(scope) {
+            scope.list = ['first','second','third'];
+        }
+    }]);
+```
+
+And the `myCustomList.html` template can be as follows:
+
+```html
+<div class="header" transclude-id="header">Default Header</div>
+<ul>
+    <li ng-repeat="item in list" transclude-id="item"></li>
+</ul>
+<div class="footer" transclude-id="footer"><hr></div>
+```
+
+
+
+Install
+-------
+
+```bash
+$ bower install --save drpx-transcludeto
+```
+
+add to your module the dependence:
+
+```javascript
+    angular.module('yourModule', ['drpxTranscludeTo']);
+```
+
+include the javascript library in your application:
+
+```html
+<script src="bower_components/drpx-transcludeto/drpx-transcludeto.js"></script>
+```
+
---
+++ drpx-transcludeto.js
@@ -0,0 +1,99 @@
+/*
+	The MIT License (MIT)
+
+	Copyright (c) 2015 David Rodenas
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+	Ex: Template of Box:
+	<div class="box">
+		<div class="header" TRANSCLUDE-ID="header"></div>
+		<hr>
+		<div class="content" TRANSCLUDE-ID="content"></div>
+	</div>
+
+	Usage of box:
+	<box>
+		<div TRANSCLUDE-TO="header">Header</div>
+		<div TRANSCLUDE-TO="content">
+			Header
+		</div>
+	</box>	
+*/
+;(function(angular) {
+	'use strict';
+
+	angular
+		.module('drpxTranscludeTo', [])
+		.value('transcludeToPreLink', transcludeToPreLink)
+		.directive('transcludeTo', transcludeToDirective)
+		.directive('transcludeId', transcludeIdDirective)
+		;
+
+	function transcludeToPreLink(preLink) {
+		return function(scope, element, attrs, controller, transclude) {
+			transclude(scope);
+
+			if (preLink) {
+				return preLink.$apply(this, arguments);
+			}
+		};
+	}
+
+	function transcludeToDirective() {
+		var directive = {
+			restrict: 'A',
+			link: { pre: preLink, },
+			transclude: 'element',
+			priority: 1600, // above ngIf, ngRepeat, ...
+			terminal: true,
+			$$tlb: true,
+		};
+
+		function preLink(scope, element, attrs, controller, transclude) {
+			scope.$transcludeTo = scope.$transcludeTo || {};
+			scope.$transcludeTo[attrs.transcludeTo] = {transclude: transclude, scope: scope.$parent};
+		}
+
+		return directive;
+	}
+
+	function transcludeIdDirective() {
+		var directive = {
+			restrict: 'A',
+			link: { pre: preLink, },
+		};
+
+		function preLink(scope, element, attrs, controller, transclude) {
+
+			var transcludeTo = scope.$transcludeTo[attrs.transcludeId];
+			if (!transcludeTo) {
+				return;
+			}
+
+			transcludeTo.transclude(transcludeTo.scope.$new(false, scope), function(clone, newScope) {
+				element.empty();
+				element.append(clone);
+			});
+		}
+
+		return directive;
+	}
+
+})(angular);
